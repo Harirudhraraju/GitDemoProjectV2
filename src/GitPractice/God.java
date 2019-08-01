afb5bb9a5fc99147e22f6ca96aafc8ab85216782
HARI RAJU <harirudhraraju2@gmail.com>
2019-08-01 11:19:13 +0530

    This is my First commit...

---
+++ src/GitPractice/God.java
@@ -0,0 +1,12 @@
+package GitPractice;
+
+public class God {
+
+	public static void main(String[] args) {
+
+		String name="OmNamassivaya";
+		char[] ch=name.toCharArray();
+		System.out.println(ch);
+	}
+
+}
